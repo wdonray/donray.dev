b96acda6d3e4670f9dba216ecb13e069299c4370
Donray Williams <donrayxwilliams@gmail.com>
2025-03-31 20:05:54 -0400

    feat: posthog and resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,51 @@
+"use client";
+
 import "./globals.css";
+import { useEffect } from "react";
+import { Router } from "next/router";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+      loaded: (posthogInstance) => {
+        if (process.env.NODE_ENV === "development") posthogInstance.debug();
+      },
+    });
+
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header />
-          {children}
-          <Footer />
-        </ThemeProvider>
+        <PostHogProvider client={posthog}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+            <Footer />
+          </ThemeProvider>
+        </PostHogProvider>
       </body>
     </html>
   );
